13a1d002ad2029099f6821a4e80c92e8e4f9f2c8
AlexRabolin <alexrabolin@yahoo.fr>
2016-10-13 00:10:53 +0200

    Update meteo-kitedor.js

--- assets/javascripts/meteo-kitedor.js
+++ assets/javascripts/meteo-kitedor.js
@@ -1,6 +1,5 @@
 (function() {
   Discourse.Markdown.whiteListTag('script', 'src', 'https://fr.windfinder.com/widget/forecast/js/*');
-  Discourse.Markdown.whiteListTag('noscript', 'a', 'href=https://www.windfinder.com/forecast/*');
   Discourse.Markdown.whiteListTag('iframe', 'src', 'https://www.meteoblue.com/fr/meteo/widget/daily/*');
   Discourse.Markdown.whiteListTag('iframe', 'src', 'http://www.windguru.cz/fr/*');
   Discourse.Markdown.whiteListTag('iframe', 'src', 'https://beta.windguru.cz/*'); 
