3d2fbb4306620f20355d01c703d45a65ba980845
AlexRabolin <alexrabolin@yahoo.fr>
2016-10-12 22:48:48 +0200

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+#name: meteo-kitedor
+ #about: Allow markdown html code for kitedor post
+ #version: 0.1
+ #authors: @Alex_Rabolin
+
+ # whitelist html tag in a post forum Discourse
+register_asset 'javascripts/meteo-kitedor', :server_side
