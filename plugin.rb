5483301664fef0915f829b659861a811c3dc5034
AlexRabolin <alexrabolin@yahoo.fr>
2016-10-14 17:54:14 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
-#name: meteo-kitedor
+#name: plugin-kitedor
  #about: Allow markdown html code for kitedor post
  #version: 0.1
  #authors: @Alex_Rabolin
 
  # whitelist html tag in a post forum Discourse
-register_asset 'javascripts/meteo-kitedor', :server_side
+register_asset 'javascripts/plugin-kitedor', :server_side
