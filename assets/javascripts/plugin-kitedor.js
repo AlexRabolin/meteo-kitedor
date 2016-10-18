5c64dd8917a91c43d9d845199e596c6e8c42169c
AlexRabolin <alexrabolin@yahoo.fr>
2016-10-19 00:23:33 +0200

    Update plugin-kitedor.js

--- assets/javascripts/plugin-kitedor.js
+++ assets/javascripts/plugin-kitedor.js
@@ -1,5 +1,6 @@
 (function() {
   Discourse.Markdown.whiteListTag('label', 'for', '*');
+  Discourse.Markdown.whiteListTag('span', 'class', '*');
   Discourse.Markdown.whiteListTag('iframe', 'src', 'https://www.meteoblue.com/fr/meteo/widget/daily/*');
   Discourse.Markdown.whiteListTag('iframe', 'src', 'http://www.windguru.cz/fr/*');
   Discourse.Markdown.whiteListTag('iframe', 'src', 'https://beta.windguru.cz/*'); 
