f37967c0430954cdb3357c09c6a136244dc23489
AlexRabolin <alexrabolin@yahoo.fr>
2017-02-04 11:28:24 +0100

    Update plugin-kitedor.js

--- assets/javascripts/plugin-kitedor.js
+++ assets/javascripts/plugin-kitedor.js
@@ -1,4 +1,6 @@
 (function () {
   Discourse.Markdown.whiteListTag('label', 'for', '*');
   Discourse.Markdown.whiteListTag('i', 'class', '*');
+  Discourse.Markdown.whiteListTag('class', '*');
+  Discourse.Markdown.whiteListTag('i', 'class', 'fa fa-*');
 })();
