18319e52af20d829b21e50680ec9556d189b5b85
AlexRabolin <alexrabolin@yahoo.fr>
2017-02-04 11:58:23 +0100

    Update plugin-kitedor.js

--- assets/javascripts/plugin-kitedor.js
+++ assets/javascripts/plugin-kitedor.js
@@ -1,6 +1,7 @@
 (function () {
   Discourse.Markdown.whiteListTag('label', 'for', '*');
-  Discourse.Markdown.whiteListTag('i', 'class', '*');
+  //Discourse.Markdown.whiteListTag('i', 'class', '*');
   Discourse.Markdown.whiteListTag('class', '*');
-  Discourse.Markdown.whiteListTag('i', 'class', 'fa fa-*');
+ // Discourse.Markdown.whiteListTag('i', 'class', 'fa fa-*');
+  //Discourse.Markdown.whiteListTag('class', '*');<span class="glyphicons glyphicons-book"></span>
 })();
